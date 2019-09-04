829648f3ccd95170ec628aa81950ae687ba4b643
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-09-04 14:45:00 +0200

    feat: auto update hint if field is empty

--- src/app/form-ingredients/wrappers/fill-in-empty-fields/fill-in-empty-fields.component.ts
+++ src/app/form-ingredients/wrappers/fill-in-empty-fields/fill-in-empty-fields.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FieldWrapper, FormlyFieldConfig } from '@ngx-formly/core';
+import { Observable } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  startWith,
+  tap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-fill-in-empty-fields',
@@ -7,10 +15,16 @@ import { FieldWrapper, FormlyFieldConfig } from '@ngx-formly/core';
   styleUrls: ['./fill-in-empty-fields.component.scss']
 })
 export class FillInEmptyFieldsComponent extends FieldWrapper implements OnInit {
-  amountOfEmptyFields = 0;
+  amountOfEmptyFields$: Observable<number> | undefined;
 
   ngOnInit() {
-    this.amountOfEmptyFields = this.countEmptyFieldsRecursive(this.field, 0);
+    this.amountOfEmptyFields$ = this.form.valueChanges.pipe(
+      startWith(0),
+      debounceTime(300),
+      distinctUntilChanged(),
+      tap(console.log),
+      map(() => this.countEmptyFieldsRecursive(this.field, 0))
+    );
   }
 
   countEmptyFieldsRecursive(field: FormlyFieldConfig, count: number): number {
